294104d4ed3e1600ef9537bc242f7370a50b810c
Mohammad Rajib <rajibkhan254051@gmail.com>
2023-06-01 16:01:25 +0600

    added PromptApp.py

---
+++ PromptApp.py
@@ -0,0 +1,34 @@
+"""
+! pip install streamlit pandasai python-dotenv
+
+Need to install above packages
+"""
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import pandas as pd
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+
+load_dotenv()
+
+API_KEY = os.environ["OPENAI_API_KEY"]
+llm = OpenAI(api_token=API_KEY)
+pandas_ai = PandasAI(llm)
+
+
+st.title("Prompt-driven analysis with PandasAI")
+uploaded_file = st.file_uploader("Upload a CSV file for analysis", type=['csv'])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head(3))
+
+    prompt = st.text_area("Enter your prompt:")
+
+    if st.button("Generate"):
+        if prompt:
+            with st.spinner("Generation response..."):
+                st.write(pandas_ai.run(df, prompt=prompt))
+        else:
+            st.warning("Please enter a prompt.")
